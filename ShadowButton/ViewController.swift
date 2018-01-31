74fb4d525d5fee3b6d7ef5695d344d7d85b11397
Piyush Sharma <piyush_sharma2@apple.com>
2018-01-30 16:42:37 -0800

    first commit

---
+++ ShadowButton/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  ShadowButton
+//
+//  Created by Piyush Sharma on 1/30/18.
+//  Copyright © 2018 Piyush Sharma. All rights reserved.
+//
+
+@IBDesignable
+class ShadowButton: UIButton {
+    
+    var shadowLayer: CAShapeLayer!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if shadowLayer == nil {
+            shadowLayer = CAShapeLayer()
+            shadowLayer.path = UIBezierPath(roundedRect: bounds, cornerRadius: 8).cgPath
+            shadowLayer.fillColor = UIColor.orange.cgColor
+            
+            shadowLayer.shadowColor = UIColor.darkGray.cgColor
+            shadowLayer.shadowPath = shadowLayer.path
+            shadowLayer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+            shadowLayer.shadowOpacity = 0.7
+            shadowLayer.shadowRadius = 2
+            
+            layer.insertSublayer(shadowLayer, at: 0)
+            //layer.insertSublayer(shadowLayer, below: nil) // also works
+        }
+    }
+}
+
+
+@IBDesignable
+class ShadowView: UIView {
+    
+    var shadowLayer: CAShapeLayer!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if shadowLayer == nil {
+            shadowLayer = CAShapeLayer()
+            shadowLayer.path = UIBezierPath(roundedRect: bounds, cornerRadius: 8).cgPath
+            shadowLayer.fillColor = UIColor.orange.cgColor
+            
+            shadowLayer.shadowColor = UIColor.darkGray.cgColor
+            shadowLayer.shadowPath = shadowLayer.path
+            shadowLayer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+            shadowLayer.shadowOpacity = 0.7
+            shadowLayer.shadowRadius = 2
+            
+            layer.insertSublayer(shadowLayer, at: 0)
+            //layer.insertSublayer(shadowLayer, below: nil) // also works
+        }
+    }
+}
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
